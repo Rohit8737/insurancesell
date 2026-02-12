463334b3da675b5203bbdac43738ecbcbb4ec969
Rohit Kashyap <roohitkashyap@gmail.com>
2026-02-12 23:12:19 +0530

    Remove AntiScrapingProtection middleware - causing 403 for visitors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->web(append: [
             \App\Http\Middleware\FilterBotTraffic::class,
-            \App\Http\Middleware\AntiScrapingProtection::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
